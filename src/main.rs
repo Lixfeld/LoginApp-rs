84f32f65c49f7a3c5c214a4ebf12bca917d66890
Lixfeld <lixfeld-git@gmx.de>
2023-04-22 17:04:46 +0200

    Rename eframe_template and TemplateApp

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "Login App",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(login_app_rs::LoginApp::new(cc))),
     )
 }
 
@@ -30,7 +30,7 @@ fn main() {
         eframe::start_web(
             "the_canvas_id", // hardcode it
             web_options,
-            Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+            Box::new(|cc| Box::new(login_app_rs::LoginApp::new(cc))),
         )
         .await
         .expect("failed to start eframe");

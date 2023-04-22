946347bcb7c66d49663aae85b5d6596d55a743af
Lixfeld <lixfeld-git@gmx.de>
2023-04-22 17:10:20 +0200

    Remove web target

--- src/main.rs
+++ src/main.rs
@@ -14,25 +14,3 @@ fn main() -> eframe::Result<()> {
         Box::new(|cc| Box::new(login_app_rs::LoginApp::new(cc))),
     )
 }
-
-// when compiling to web using trunk.
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    // Make sure panics are logged using `console.error`.
-    console_error_panic_hook::set_once();
-
-    // Redirect tracing to console.log and friends:
-    tracing_wasm::set_as_global_default();
-
-    let web_options = eframe::WebOptions::default();
-
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::start_web(
-            "the_canvas_id", // hardcode it
-            web_options,
-            Box::new(|cc| Box::new(login_app_rs::LoginApp::new(cc))),
-        )
-        .await
-        .expect("failed to start eframe");
-    });
-}

1fbcfe84d736459447541674c78da55bf839279b
Lixfeld <lixfeld-git@gmx.de>
2023-04-22 18:17:15 +0200

    Add basic login controls

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,16 @@
-pub struct LoginApp {}
+pub struct LoginApp {
+    username: String,
+    password: String,
+    confirm_password: String,
+}
 
 impl Default for LoginApp {
     fn default() -> Self {
-        Self {}
+        Self {
+            username: String::new(),
+            password: String::new(),
+            confirm_password: String::new(),
+        }
     }
 }
 
@@ -20,8 +28,25 @@ impl eframe::App for LoginApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let Self {
+            username,
+            password,
+            confirm_password,
+        } = self;
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            todo!();
+            ui.label("Username:");
+            ui.text_edit_singleline(username);
+
+            ui.label("Password:");
+            ui.text_edit_singleline(password);
+
+            ui.label("Confirm password:");
+            ui.text_edit_singleline(confirm_password);
+
+            let is_empty = username.is_empty() || password.is_empty();
+            let is_match = password.clone() == confirm_password.clone();
+            ui.add_enabled(!is_empty && is_match, egui::Button::new("Sign up"));
         });
     }
 }
